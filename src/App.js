bcf749c19402cd4d5d3c9dada24a5162c1dfdc2d
rajesh <rajesh@e-arth.in>
2022-03-19 11:46:15 +0530

    scr app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Container, NavBar } from "./components";
+import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
